6cd63035eff56db6de89a9e2ae418cf2eac61287
Flora Martins <floramartins@Floras-MBP.lan>
2023-09-25 09:47:02 -0300

    add details to pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,10 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+let currentOpenPokemon = null;
 
 function convertPokemonToLi(pokemon) {
+
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
@@ -19,29 +21,86 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <div class="details">
+                <div>
+                    <p><b>Weight</b> ${pokemon.weight}</p>
+                    <p><b>Height</b> ${pokemon.height}</p>
+                </div>
+                <div>
+                    <p><b>Abilities</b></p>
+                    <ul class="abilities">
+                        ${pokemon.abilities.map((ability) => `<li class="ability">${ability}</li>`).join('')}
+                    </ul>
+                </div>
+                ${pokemon.heldItems.length ?
+                    `<div>
+                        <p><b>Held Items</b></p>
+                        <ul class="abilities">
+                            ${pokemon.heldItems.map((heldItem) => `<li class="ability">${heldItem}</li>`).join('')}
+                        </ul>
+                    </div>`
+                : ''}
+            </div>
         </li>
     `
 }
 
-function loadPokemonItens(offset, limit) {
+
+function loadPokemonItems(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
-
-loadPokemonItens(offset, limit)
-
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
-
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+      const newHtml = pokemons.map(convertPokemonToLi).join('');
+      pokemonList.innerHTML += newHtml;
+      
+      addClickListeners();
+    });
+  }
+  
+  function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToLi).join('');
+      pokemonList.innerHTML += newHtml;
+      
+      addClickListeners();
+    });
+  }
+  
+  function addClickListeners() {
+    const pokemonElements = document.querySelectorAll('.pokemon');
+    pokemonElements.forEach(function (element) {
+      element.addEventListener('click', function () {
+        if (currentOpenPokemon) {
+          currentOpenPokemon.classList.remove('open');
+        }
+        
+        this.classList.add('open');
+        
+        currentOpenPokemon = this;
+      });
+    });
+  }
+  
+  loadPokemonItems(offset, limit);
+  addClickListeners();
+  
+  loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithNextPage = offset + limit;
+  
+    if (qtdRecordsWithNextPage >= maxRecords) {
+      const newLimit = maxRecords - offset;
+      loadPokemonItems(offset, newLimit);
+      loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
+      loadPokemonItems(offset, limit);
     }
-})
+  });
+  
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.addedNodes) {
+        addClickListeners();
+      }
+    });
+  });
+  
+  observer.observe(pokemonList, { childList: true });
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,12 +5,27 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+    pokemon.weight = pokeDetail.weight
+    pokemon.height = pokeDetail.height
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
+
+    const abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+    const [ability] = abilities
+
+    pokemon.abilities = abilities
+    pokemon.ability = ability
+
+    const heldItems = pokeDetail.held_items.map((item) => item.item.name)
+    const [heldItem] = heldItems
+
+    pokemon.heldItems = heldItems
+    pokemon.heldItem = heldItem
+
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
